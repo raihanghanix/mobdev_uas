4669a45b6a39b2f3f53ee2cbdf2f65a4fca6fffa
unknown <raihanghani58@gmail.com>
2023-12-19 19:32:35 +0700

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,62 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'Services/notifi_service.dart';
+import 'package:local_notification_app_demo/Services/notifi_service.dart';
+import 'package:local_notification_app_demo/routes/home.dart';
+import 'package:local_notification_app_demo/routes/login.dart';
+import 'package:local_notification_app_demo/utils/theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:local_notification_app_demo/utils/file_controller.dart';
+import 'package:local_notification_app_demo/widgets/loading.dart';
 
-import 'home_page.dart';
-
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService().initNotification();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<dynamic> _checkFile(BuildContext context) async {
+    final file = await FileController(context: context).readFile("user.json");
+    if (file.isEmpty) return null;
+    final Map<String, dynamic> json = await jsonDecode(file);
+    final userId = json['userId'];
+    return userId;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Notifications',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Productiv',
+      home: FutureBuilder(
+        future: _checkFile(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState.name == 'waiting') {
+            return const Scaffold(
+              body: Center(
+                child: LoadingIndicator(),
+              ),
+            );
+          }
+          return Scaffold(
+            body: snapshot.data != null
+                ? Home(
+                    userId: snapshot.data,
+                  )
+                : const Login(),
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Local Notifications'),
+      theme: applyTheme(),
     );
   }
 }
